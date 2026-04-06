85634b37a09da12e7fedb2067ccd6d3c5c6a3547
mikhaildadaev <mikhaildadaev@gmail.com>
2026-04-06 18:29:28 +0300

    Обновление бенчмарков

--- ulog_bench_test.go
+++ ulog_bench_test.go
@@ -7,44 +7,108 @@ import (
 
 // Бенчмарки компонентов
 func BenchmarkDebug(b *testing.B) {
-	logger := New()
-	logger.Debug("test debug message")
-	logger.SetOutput(io.Discard)
-	defer logger.Sync()
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
+	b.Run("Simple", func(b *testing.B) {
+		logger := New()
 		logger.Debug("test debug message")
-	}
+		logger.SetOutput(io.Discard)
+		defer logger.Sync()
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			logger.Debug("test debug message")
+		}
+	})
+	b.Run("Format", func(b *testing.B) {
+		logger := New()
+		logger.Debug("test debug message")
+		logger.SetOutput(io.Discard)
+		defer logger.Sync()
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			logger.Debug("test debug message",
+				String("code", "ERR_001"),
+				Int("user_id", 12345),
+				String("path", "/api/v1/test"),
+			)
+		}
+	})
 }
 func BenchmarkError(b *testing.B) {
-	logger := New()
-	logger.Error("test error message")
-	logger.SetOutput(io.Discard)
-	defer logger.Sync()
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
+	b.Run("Simple", func(b *testing.B) {
+		logger := New()
 		logger.Error("test error message")
-	}
+		logger.SetOutput(io.Discard)
+		defer logger.Sync()
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			logger.Error("test error message")
+		}
+	})
+	b.Run("Format", func(b *testing.B) {
+		logger := New()
+		logger.Error("test error message")
+		logger.SetOutput(io.Discard)
+		defer logger.Sync()
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			logger.Error("test error message",
+				String("code", "ERR_001"),
+				Int("user_id", 12345),
+				String("path", "/api/v1/test"),
+			)
+		}
+	})
 }
 func BenchmarkInfo(b *testing.B) {
-	logger := New()
-	logger.Info("test info message")
-	logger.SetOutput(io.Discard)
-	defer logger.Sync()
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
+	b.Run("Simple", func(b *testing.B) {
+		logger := New()
 		logger.Info("test info message")
-	}
+		logger.SetOutput(io.Discard)
+		defer logger.Sync()
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			logger.Info("test info message")
+		}
+	})
+	b.Run("Format", func(b *testing.B) {
+		logger := New()
+		logger.Info("test info message")
+		logger.SetOutput(io.Discard)
+		defer logger.Sync()
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			logger.Info("test info message",
+				String("code", "ERR_001"),
+				Int("user_id", 12345),
+				String("path", "/api/v1/test"),
+			)
+		}
+	})
 }
 func BenchmarkWarn(b *testing.B) {
-	logger := New()
-	logger.Warn("test warn message")
-	logger.SetOutput(io.Discard)
-	defer logger.Sync()
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
+	b.Run("Simple", func(b *testing.B) {
+		logger := New()
 		logger.Warn("test warn message")
-	}
+		logger.SetOutput(io.Discard)
+		defer logger.Sync()
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			logger.Warn("test warn message")
+		}
+	})
+	b.Run("Format", func(b *testing.B) {
+		logger := New()
+		logger.Warn("test warn message")
+		logger.SetOutput(io.Discard)
+		defer logger.Sync()
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			logger.Warn("test warn message",
+				String("code", "ERR_001"),
+				Int("user_id", 12345),
+				String("path", "/api/v1/test"),
+			)
+		}
+	})
 }
 func BenchmarkLoggerWriter(b *testing.B) {
 	logger := New()
