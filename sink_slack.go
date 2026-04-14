37a57c2ba17721a974515160b8c4a9bd92318656
mikhaildadaev <mikhaildadaev@gmail.com>
2026-04-14 16:13:06 +0300

    Обновление sink_slack.go

--- sink_slack.go
+++ sink_slack.go
@@ -3,105 +3,34 @@
 package ulog
 
 import (
-	"bytes"
 	"encoding/json"
-	"fmt"
-	"net/http"
-	"time"
 )
 
 // Публичные структуры
-type SlackSink struct {
-	channel    string
-	client     *http.Client
-	iconEmoji  string
-	iconURL    string
-	minLevel   TypeLevel
-	username   string
-	webhookURL string
+type SlackData struct {
+	Channel   string `json:"channel,omitempty"`
+	IconEmoji string `json:"icon_emoji,omitempty"`
+	IconURL   string `json:"icon_url,omitempty"`
+	Text      string `json:"text"`
+	UserName  string `json:"username,omitempty"`
 }
-type SlackOption func(*SlackSink)
+type SlackSink = HttpSink
 
 // Публичные конструкторы
-func NewSlackSink(minLevel TypeLevel, webhookURL string, options ...SlackOption) *SlackSink {
-	sink := &SlackSink{
-		client:     &http.Client{Timeout: 10 * time.Second},
-		minLevel:   minLevel,
-		webhookURL: webhookURL,
-	}
-	for _, option := range options {
-		option(sink)
-	}
-	return sink
-}
-
-// Публичные функции
-func WithSlackChannel(channel string) SlackOption {
-	return func(slackSink *SlackSink) {
-		slackSink.channel = channel
-	}
-}
-func WithSlackIconEmoji(emoji string) SlackOption {
-	return func(slackSink *SlackSink) {
-		slackSink.iconEmoji = emoji
-	}
-}
-func WithSlackIconURL(url string) SlackOption {
-	return func(slackSink *SlackSink) {
-		slackSink.iconURL = url
-	}
-}
-func WithSlackTimeout(timeout time.Duration) SlackOption {
-	return func(slackSink *SlackSink) {
-		slackSink.client.Timeout = timeout
-	}
-}
-func WithSlackUsername(username string) SlackOption {
-	return func(slackSink *SlackSink) {
-		slackSink.username = username
-	}
-}
-
-// Публичные методы
-func (slackSink *SlackSink) Close() error {
-	slackSink.client.CloseIdleConnections()
-	return nil
-}
-func (slackSink *SlackSink) Write(p []byte) (n int, err error) {
-	msg := string(p)
-	if len(msg) > maxSlackMessageLen {
-		msg = msg[:maxSlackMessageLen-3] + "..."
-	}
-	webhook := struct {
-		Text      string `json:"text"`
-		Username  string `json:"username,omitempty"`
-		IconEmoji string `json:"icon_emoji,omitempty"`
-		IconURL   string `json:"icon_url,omitempty"`
-		Channel   string `json:"channel,omitempty"`
-	}{
-		Text:      msg,
-		Username:  slackSink.username,
-		IconEmoji: slackSink.iconEmoji,
-		IconURL:   slackSink.iconURL,
-		Channel:   slackSink.channel,
-	}
-	jsonBody, err := json.Marshal(webhook)
-	if err != nil {
-		return 0, fmt.Errorf("slack marshal: %w", err)
-	}
-	resp, err := slackSink.client.Post(slackSink.webhookURL, "application/json", bytes.NewReader(jsonBody))
-	if err != nil {
-		return 0, fmt.Errorf("slack post: %w", err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-		return 0, fmt.Errorf("slack: %s", resp.Status)
-	}
-	return len(p), nil
-}
-func (slackSink *SlackSink) WriteWithLevel(level TypeLevel, p []byte) (n int, err error) {
-	if level < slackSink.minLevel {
-		return len(p), nil
-	}
-	return slackSink.Write(p)
+func NewSlackSink(endPoint, userName, iconEmoji, iconURL, channel string, options ...HttpOption) *HttpSink {
+	return NewHttpSink(endPoint, append([]HttpOption{
+		WithHttpFormatter(func(level TypeLevel, p []byte) ([]byte, error) {
+			data := SlackData{
+				Channel:   channel,
+				IconEmoji: iconEmoji,
+				IconURL:   iconURL,
+				Text:      string(p),
+				UserName:  userName,
+			}
+			return json.Marshal(data)
+		}),
+		WithHttpHeader("Content-Type", "application/json"),
+		WithHttpLevelMin(LevelError),
+		WithHttpMethod("POST"),
+	}, options...)...)
 }
