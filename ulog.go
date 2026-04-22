7adde5ca648f318ab0a6aab2887e1fd3cdeb31b9
mikhaildadaev <mikhaildadaev@gmail.com>
2026-04-22 11:13:00 +0300

    Обновление лицензии

--- ulog.go
+++ ulog.go
@@ -1,4 +1,4 @@
-// Copyright [2026] Mikhail Dadaev
+// Copyright [2026] [Mikhail Dadaev]
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
