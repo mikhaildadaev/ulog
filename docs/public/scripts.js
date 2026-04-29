8b5759b2ee4bcf7f7c959440938c82af752945af
mikhaildadaev <mikhaildadaev@gmail.com>
2026-04-29 04:19:18 +0300

    Обновление документации

--- docs/public/scripts.js
+++ docs/public/scripts.js
@@ -1,5 +1,23 @@
 (function() {
   'use strict';
+  const savedLang = localStorage.getItem('vitepress-lang');
+  const currentPath = window.location.pathname;
+  const supportedLangs = ['en', 'ru', 'zh'];
+  if (savedLang && supportedLangs.includes(savedLang)) {
+    const prefix = '/' + savedLang + '/';
+    if (!currentPath.startsWith(prefix)) {
+      let rest = currentPath;
+      for (const l of supportedLangs) {
+        const p = '/' + l + '/';
+        if (currentPath.startsWith(p)) {
+          rest = currentPath.substring(p.length - 1);
+          break;
+        }
+      }
+      window.location.replace(prefix + rest.replace(/^\//, ''));
+      return;
+    }
+  }
   const lang = document.documentElement.lang;
   if (lang) {
     localStorage.setItem('vitepress-lang', lang);
